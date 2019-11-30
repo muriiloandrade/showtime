0d6007480c59dbd9d0619f645fa73af7676c8e53
Murilo A. 🎧 <muriloandradealmeida@gmail.com>
2019-11-29 21:42:19 -0300

    Navbar, footer and home implemented plus some packages added to the project

--- showtime-front/src/app/app-routing.module.ts
+++ showtime-front/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FilmesComponent } from './components/filmes/filmes.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- showtime-front/src/app/app.module.ts
+++ showtime-front/src/app/app.module.ts
@@ -4,9 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { FilmesComponent } from './components/filmes/filmes.component';
+import { HomeComponent } from './components/home/home.component';
+import { TestimoniesComponent } from './components/testimonies/testimonies.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    FilmesComponent,
+    HomeComponent,
+    TestimoniesComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
