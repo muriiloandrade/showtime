5bdc22621d653f236decdf84fe8c8479b9fce42e
Murilo A. 🎧 <muriloandradealmeida@gmail.com>
2019-12-01 02:56:06 -0300

    Small adjustments

--- showtime-front/src/app/components/login/login.module.ts
+++ showtime-front/src/app/components/login/login.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule, RouterModule],
   exports: [LoginComponent],
 })
 export class LoginModule {}
